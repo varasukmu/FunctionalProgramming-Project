bdc54bec0e92c0ce81fc65098b97475eabc3b182
Ttuch <almon025@gmail.com>
2026-03-08 21:26:03 +0700

    Split Date to day month year
    Create DateProcessor.scala

--- final-project/src/main/scala/Main.scala
+++ final-project/src/main/scala/Main.scala
@@ -5,9 +5,20 @@
   extractAccidentData(fileName) match
     case Right(data) =>
       println(s"Success! Found ${data.size} clean records.")
-      data.take(100).foreach { rec =>
-        println(s"Age: ${rec.age} | Sex: ${rec.sex} | Resident: ${rec.homeLoc} | Scene: ${rec.incidentLoc} | Date: ${rec.deadDate}")
-      }
+
+      // เรียก function แยกวันเดือนปี
+      val dateprocessed =
+        data.flatMap(DataProcessor.splitDate)
+
+      // data.take(100).foreach { rec =>
+      //   println(s"Age: ${rec.age} | Sex: ${rec.sex} | Resident: ${rec.homeLoc} | Scene: ${rec.incidentLoc} | Date: ${rec.deadDate}")
+      // }
       
+      dateprocessed.take(100).foreach { rec =>
+        println(s"Age: ${rec.age} | Sex: ${rec.sex} | Resident: ${rec.homeLoc} | Scene: ${rec.incidentLoc} | Date: ${rec.deadDate} | Day: ${rec.day} | Month: ${rec.month} | Year: ${rec.year}")
+      }
+
+      println(s"Records with valid date: ${dateprocessed.size}")
+
     case Left(error) => 
       System.err.println(error)
